700a7638cce65e4bb838f4553ec8b7faf826ab52
Grayb3 <97465236+Grayb3@users.noreply.github.com>
2022-04-21 21:48:14 -0400

    creating GUI
    Begin the creation of the GUI menu for the test so that it can create an interactive menu so the user has a better experience. Here you can see I played around with some commands that will build this foundation.

--- GUI.py
+++ GUI.py
@@ -1,1 +1,21 @@
-import Tkinter
+from tkinter import *
+from tkinter.ttk import * 
+
+
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
+
+root = Tk()
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Record spending/earning", command=donothing)
+filemenu.add_command(label="View Finances", command=donothing)
+filemenu.add_command(label="Quit", command=quit)
+
+filemenu.add_separator()
+
+
+root.config(menu=menubar)
+root.mainloop()
